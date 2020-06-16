a425948c009d90d388624cba48d3e10e31debfbb
Manash <manash@cefalo.com>
2020-06-16 22:23:48 +0600

    REST API: Craete very basic REST API endpoints for list, single & create resource

---
+++ rest_api/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); // To use req.body
+const courses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' }
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    //req.params.id //Get Route parameter
+    //req.query //Get Query parameter
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course withe the given ID was not found');
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
