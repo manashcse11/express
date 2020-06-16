140e8826af60bd0d1d226c62905a2c09b1661659
Manash <manash@cefalo.com>
2020-06-16 22:50:39 +0600

    REST API: Add validation

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,6 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 app.use(express.json()); // To use req.body
+
 const courses = [
     { id: 1, name: 'course 1' },
     { id: 2, name: 'course 2' },
@@ -24,6 +26,17 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
